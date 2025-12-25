96e2e9415d29d023a50a4552c226deadc7e9adf5
Den R) <90736052+Chisato-Nishikigi@users.noreply.github.com>
2025-12-25 14:59:49 +0900

    Update build.gradle.kts

--- XprimeProvider/build.gradle.kts
+++ XprimeProvider/build.gradle.kts
@@ -1,6 +1,27 @@
+plugins {
+    id("cloudstream.plugin")
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
+
+version = 1
+
 cloudstream {
     description = "Movies & TV from XPrime"
     authors = listOf("Reno")
+
+    status = 1
     tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
     language = "en"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
 }
