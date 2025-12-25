d6198990f9e3af65d669f673e32d44d1698f7dbc
Den R) <90736052+Chisato-Nishikigi@users.noreply.github.com>
2025-12-25 15:12:03 +0900

    Update build.gradle.kts

--- XprimeProvider/build.gradle.kts
+++ XprimeProvider/build.gradle.kts
@@ -3,31 +3,16 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    description = "Movies & TV from XPrime"
+    authors = listOf("Reno")
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
     language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
 android {
