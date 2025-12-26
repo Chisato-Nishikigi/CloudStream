75a2b2a38d47df1bcdb749c59330ebf30097ad23
Den R) <90736052+Chisato-Nishikigi@users.noreply.github.com>
2025-12-26 15:09:13 +0900

    Update XprimePlugin.kt

--- XprimeProvider/src/main/kotlin/XprimePlugin.kt
+++ XprimeProvider/src/main/kotlin/XprimePlugin.kt
@@ -1,11 +1,12 @@
-package com.hexated
+package com.yourname.xprime
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.MainAPI
 
 @CloudstreamPlugin
 class XprimePlugin : Plugin() {
-    override fun load() {
-        registerMainAPI(XprimeProvider())
+    override fun load(context: Context) {
+        registerMainAPI(XPrimeProvider())
     }
 }
